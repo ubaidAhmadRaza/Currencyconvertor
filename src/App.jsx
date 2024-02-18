d1be2582acb4c82721b064b00d0f2487da9d8113
Ubaid Ahmad Raza <ubaidahmad3006@gmail.com>
2024-02-19 00:20:39 +0500

    index.html and icon remove

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState,useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import Input from './Components/Input';
 import useCurrencyInfo from './Hooks/currency';
