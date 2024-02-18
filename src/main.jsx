88c02f4de0df83fc10d65dbc287a2ebd3e5e93ec
Ubaid Ahmad Raza <ubaidahmad3006@gmail.com>
2024-02-19 00:47:25 +0500

    speedinsights

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { inject } from '@vercel/analytics';
+import { injectSpeedInsights } from '@vercel/speed-insights';
+ 
+injectSpeedInsights();
  
 inject();
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
